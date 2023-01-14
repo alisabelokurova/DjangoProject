b4295f7492452b7f1a2ba967521bf906f91e5297
alisabelokurova <alisabelokurova252@gmail.com>
2023-01-14 12:51:59 +0300

    работа с ORM часть 1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse, Http404
+from django.contrib.auth.models import User
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
 
 def home(request):
     # text = f""" <h1>"Изучаем django"</h1> <strong>Автор</strong>: <i>Лазуренко Н.С.</i>"""
@@ -8,14 +11,15 @@ def home(request):
 def about(request):
     return render(request,"about.html")
 
-items = [
-   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-   {"id": 3, "name": "Coca-cola 1 литр" ,"quantity":12},
-   {"id": 4, "name": "Картофель фри" ,"quantity":0},
-   {"id": 5, "name": "Кепка" ,"quantity":124},
-]
+# items = [
+#    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+#    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+#    {"id": 3, "name": "Coca-cola 1 литр" ,"quantity":12},
+#    {"id": 4, "name": "Картофель фри" ,"quantity":0},
+#    {"id": 5, "name": "Кепка" ,"quantity":124},
+# ]
 def items_list(request):
+    items = Item.objects.all()
     # items_result = "<ol>"
     # for item in items:
     #     items_result += "<li>" + f"<a href ='/item/{item['id']}'> " + item['name'] + "</a>" + "</li>"
@@ -31,10 +35,10 @@ def items_list(request):
 
 
 def item_page(request,id):
-    for item in items:
-        if item['id'] == id:
-    #         item_str = f"товар {item['name']} количество{item['quantity']}"
-    #         return HttpResponse(item_str)
-    # return HttpResponse(f"Товар с id = {id} не найден")
-            return render(request,"item_page.html", item)
-    raise Http404(f"Товар с id = {id} не найден")
+    try:
+        item = Item.objects.get(pk=id)
+    # for item in items:
+    #     if item['id'] == id:
+        return render(request,"item_page.html", {"item":item})
+    except ObjectDoesNotExist:
+        raise Http404(f"Товар с id = {id} не найден")
